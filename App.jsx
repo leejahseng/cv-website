540b2661d82762764fe3e1c38d9c8347fa593d81
Jah Seng Lee <jahseng619@gmail.com>
2016-07-11 16:30:46 +1200

    added a SideBar component

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SideBar from './src/sidebar/SideBar.jsx';
 
 class App extends React.Component {
     render() {
         return (
             <div>
+                <SideBar />
                 Hello World!
             </div>
         );
