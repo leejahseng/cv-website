d78e5c0f762f625a797c863c1c958d6ce729ca05
Jah Seng Lee <jahseng619@gmail.com>
2016-09-20 23:23:47 +1200

    Added icons to side-bar

--- src/components/SideBar/NavButton.jsx
+++ src/components/SideBar/NavButton.jsx
@@ -4,14 +4,19 @@ export default class NavButton extends React.Component{
 
     constructor(props) {
         super(props);
-        this.state = {name: props.name, href: props.href};
+        this.state = {
+            name: props.name,
+            href: props.href,
+            icon: props.icon
+        };
     }
 
     render() {
         return(
             <div className='nav-button' >
                 <a href={this.state.href} >
-                    {this.state.name}
+                    <p>{this.state.name}</p>
+                    {this.state.icon}
                 </a>
             </div>
         )
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import NavButton from './NavButton.jsx'
 
+// Icons - must be better way of importing
+var FaUser = require('react-icons/lib/fa/user');
+var User = <FaUser className="icon"/>;
+var FaFileText = require('react-icons/lib/fa/file-text');
+var FileText = <FaFileText className="icon"/>;
+var FaPaintBrush = require('react-icons/lib/fa/paint-brush');
+var PaintBrush = <FaPaintBrush className="icon"/>;
+var FaEllipsis = require('react-icons/lib/fa/ellipsis-h');
+var Ellipsis = <FaEllipsis className="icon"/>;
+
 export default class SideBar extends React.Component{
-    
+
     render() {
         return(
             <div className='side-bar'>
-                <NavButton name="About Me" href="about-me"/>
-                <NavButton name="Programming" href="programming"/>
-                <NavButton name="Art" href="art"/>
-                <NavButton name="More" href="more"/>
+                <NavButton name="About Me" href="about-me" icon={User}/>
+                <NavButton name="Programming" href="programming" icon={FileText}/>
+                <NavButton name="Art" href="art" icon={PaintBrush}/>
+                <NavButton name="More" href="more" icon={Ellipsis}/>
             </div>
         )
     }
